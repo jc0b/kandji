ac8c81028bfccc0e7b1ebfaf7ab38a3dba04a00b
Jacob Burley <j@jc0b.computer>
2022-09-26 16:58:42 +0200

    Moving to Eloquent

---
+++ kandji_processor.php
@@ -0,0 +1,27 @@
+<?php
+
+use munkireport\processors\Processor;
+
+class Kandji_new_processor extends Processor
+{
+    public function run($data)
+    {   
+        configAppendFile(__DIR__ . '/config.php');
+        $modelData = ['serial_number' => $this->serial_number];
+        $module_dir = dirname(__FILE__);
+        // Check if we should enable Kandji lookup
+        if (conf('kandji_enable')) {
+            // Load Kandji helper
+            require_once($module_dir.'/lib/kandji_helper.php');
+            $kandji_helper = new munkireport\module\kandji\kandji_helper;
+            $kandji_helper->pull_kandji_data($modelData);
+            // ^^ Comment and uncomment to turn off and on
+        }
+
+        Kandji_model::updateOrCreate(
+            ['serial_number' => $this->serial_number], $modelData
+        );
+
+        return $this;
+    }
+}
