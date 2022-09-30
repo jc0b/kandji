314864bea55c8be1438225324515549e3f57dffd
Jacob Burley <j@jc0b.computer>
2022-09-30 15:34:37 +0200

    Testing kandji_version

--- kandji_controller.php
+++ kandji_controller.php
@@ -40,7 +40,7 @@ class kandji_controller extends Module_controller
     public function get_kandji_version()
     {   
         // $kandji_version_data = Kandji_model::selectRaw("COALESCE(SUM(CASE WHEN kandji_agent_version <> '' AND kandji_agent_version IS NOT NULL THEN 1 END), 0) AS count, kandji_agent_version")->filter()->groupBy('kandji_agent_version')->orderBy('count', 'desc');
-        $kandji_version_data = Kandji_model::selectRaw("COALESCE(SUM(CASE WHEN kandji_agent_version IS NOT NULL THEN 1 END), 0) AS count, kandji_agent_version");
+        $kandji_version_data = Kandji_model::selectRaw("COALESCE(SUM(CASE WHEN kandji_agent_version IS NOT NULL THEN 1 END), 0) AS count, kandji_agent_version")->filter()->groupBy('kandji_agent_version')->orderBy('count', 'desc')->get()->toArray();
         // $sql = "SELECT COUNT(CASE WHEN kandji_agent_version <> '' AND kandji_agent_version IS NOT NULL THEN 1 END) AS count, kandji_agent_version 
         //         FROM kandji
         //         LEFT JOIN reportdata USING (serial_number)
