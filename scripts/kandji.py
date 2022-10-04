79714698bec08e69da56f466ec4a883deea9a2cd
Jacob Burley <j@jc0b.computer>
2022-10-04 16:59:08 +0200

    Return result

--- scripts/kandji.py
+++ scripts/kandji.py
@@ -17,6 +17,7 @@ def get_local_kandji_prefs():
     result['blueprint_name'] = CFPreferencesCopyAppValue('AgentVersion', 'io.kandji.Kandji')
     result['kandji_agent_version'] = CFPreferencesCopyAppValue('Blueprint', 'io.kandji.Kandji')
     result['kandji_id'] = CFPreferencesCopyAppValue('ComputerURL', 'io.kandji.Kandji').split('/')[-1]
+    return result
 
 def kandji_report_enabled():
     return CFPreferencesCopyAppValue('KandjiCheck', 'MunkiReport')
