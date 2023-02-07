d12d51143c28eeb875957eb620edba26115facbf
jc0b <j@jc0b.computer>
2023-02-07 10:57:10 +0100

    Updated migration file

--- migrations/2023_02_07_000002_kandji.php
+++ migrations/2023_02_07_000002_kandji.php
@@ -6,10 +6,24 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 class Kandji extends Migration
 {
     private $tableName = 'kandji';
+    private $tableNameV2 = 'kandji_orig';
     
     public function up()
     {
         $capsule = new Capsule();
+
+        $migrateData = false;
+
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            // Migration already failed before, but didnt finish
+            throw new Exception("previous failed migration exists");
+        }
+
+        if ($capsule::schema()->hasTable($this->tableName)) {
+            $capsule::schema()->rename($this->tableName, $this->tableNameV2);
+            $migrateData = true;
+        }
+
         $capsule::schema()->create($this->tableName, function (Blueprint $table) {
             $table->increments('id');
             $table->string('serial_number')->unique();
@@ -27,6 +41,30 @@ class Kandji extends Migration
             $table->string('passport_enabled')->nullable();
             $table->string('passport_users')->nullable();
         });
+
+        if ($migrateData) {
+            $capsule::unprepared("INSERT INTO 
+                $this->tableName
+            SELECT
+                id,
+                serial_number,
+                kandji_id,
+                name,
+                kandji_agent_version,
+                asset_tag,
+                last_check_in,
+                last_enrollment,
+                first_enrollment,
+                blueprint_id,
+                blueprint_name,
+                realname,
+                email_address,
+                passport_enabled,
+                passport_users
+            FROM
+                $this->tableNameV2");
+            $capsule::schema()->drop($this->tableNameV2);
+        }
         
         // Make the indexes
         $capsule::schema()->table($this->tableName, function (Blueprint $table) {
@@ -45,5 +83,8 @@ class Kandji extends Migration
     {
         $capsule = new Capsule();
         $capsule::schema()->dropIfExists($this->tableName);
+        if ($capsule::schema()->hasTable($this->tableNameV2)) {
+            $capsule::schema()->rename($this->tableNameV2, $this->tableName);
+        }
     }
 }
